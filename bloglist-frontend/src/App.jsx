e1eaf579ee18f5f6c7b678b6d9d3be1d9dbb2d55
Jarttu <Jaahas.ihminen@gmail.com>
2026-05-08 15:44:23 +0300

    5.10

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -149,7 +149,7 @@ const App = () => {
 				<BlogForm createBlog={handleCreate} />
 			</Togglable>
 
-			{blogs.map(blog =>
+			{blogs.sort((a, b) => b.likes - a.likes).map(blog =>
 				<Blog key={blog.id} blog={blog} handleLike={handleLike} />
 			)}
 		</div>
