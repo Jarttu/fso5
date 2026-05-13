4c4b08de5b78be2167a34794a7401b3d4cc10a42
Jarttu <Jaahas.ihminen@gmail.com>
2026-05-13 04:56:04 +0300

    5.19

--- e2e/tests/blog_app.spec.js
+++ e2e/tests/blog_app.spec.js
@@ -35,4 +35,24 @@ describe('Blog app', () => {
             await expect(page.getByText('Wrong username or password')).toBeVisible()
         })
     })
+    describe('When logged in', () => {
+        beforeEach(async ({ page }) => {
+            await page.getByRole('textbox').first().fill('testuser')
+            await page.getByRole('textbox').last().fill('testpassword')
+            await page.getByRole('button', { name: 'login' }).click()
+
+            await expect(page.getByText('Test User logged in')).toBeVisible()
+        })
+        test('A blog can be created', async ({ page }) => {
+            await page.getByRole('button', { name: 'create new blog' }).click()
+
+            await page.getByRole('textbox').first().fill('e2eTest Blog')
+            await page.getByRole('textbox').nth(1).fill('e2eTest Blog Author')
+            await page.getByRole('textbox').nth(2).fill('http://e2etestblogurl.com')
+            await page.getByRole('button', { name: 'create' }).click()
+
+            await expect(page.getByText('e2eTest Blog').first()).toBeVisible()
+            await expect(page.getByText('e2eTest Blog Author').first()).toBeVisible()
+        })
+    })
 })
